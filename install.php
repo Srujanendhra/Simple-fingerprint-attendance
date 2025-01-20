9bc23109c8b3113bf0e169c2b099f8004a23564d
Srujanendhra <103301943+Srujanendhra@users.noreply.github.com>
2025-01-20 23:49:06 +0530

    install.php

--- install.php
+++ install.php
@@ -8,7 +8,7 @@
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
 	// Create database
-	$sql = "CREATE DATABASE biometricattendace";
+	$sql = "CREATE DATABASE Finger_Attendance";
 	if ($conn->query($sql) === TRUE) {
 	    echo "Database created successfully";
 	} else {
@@ -17,43 +17,21 @@
 
 	echo "<br>";
 
-	$dbname = "biometricattendace";
+	$dbname = "Finger_Attendance";
     
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
 	// sql to create table
-	$sql = "CREATE TABLE IF NOT EXISTS `users` (
-			`id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-			`username` varchar(100) NOT NULL,
-			`serialnumber` double NOT NULL,
-			`gender` varchar(10) NOT NULL,
-			`email` varchar(50) NOT NULL,
-			`fingerprint_id` int(11) NOT NULL,
-			`fingerprint_select` tinyint(1) NOT NULL DEFAULT '0',
-			`user_date` date NOT NULL,
-			`time_in` time NOT NULL,
-			`del_fingerid` tinyint(1) NOT NULL DEFAULT '0',
-			`add_fingerid` tinyint(1) NOT NULL DEFAULT '0'
+	$sql = "CREATE TABLE IF NOT EXISTS `Attendance` (
+			`SNo.` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			`Name` varchar(30) NOT NULL,
+			`Finger_ID` int(11) NOT NULL,
+			`Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			`Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 	) ENGINE=InnoDB DEFAULT CHARSET=latin1";
 
 	if ($conn->query($sql) === TRUE) {
 	    echo "Table users created successfully";
-	} else {
-	    echo "Error creating table: " . $conn->error;
-	}
-
-	$sql = "CREATE TABLE IF NOT EXISTS `users_logs` (
-			`id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-			`username` varchar(100) NOT NULL,
-			`serialnumber` double NOT NULL,
-			`fingerprint_id` int(5) NOT NULL,
-			`checkindate` date NOT NULL,
-			`timein` time NOT NULL,
-			`timeout` time NOT NULL
-	) ENGINE=InnoDB DEFAULT CHARSET=latin1";
-
-	if ($conn->query($sql) === TRUE) {
-	    echo "Table users_logs created successfully";
 	} else {
 	    echo "Error creating table: " . $conn->error;
 	}
